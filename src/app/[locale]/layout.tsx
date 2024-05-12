01f76f7c1d5f1a80bd6e12c61f85d15bc3b8ca0c
Sedrakable <sedrak005@gmail.com>
2024-05-12 18:31:42 -0400

    added facebook ads

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import styles from "./layout.module.scss";
 import "@/styles/Main.css";
 import "@/styles/ScrollBar.scss";
@@ -76,6 +78,37 @@ export default async function LocaleLayout({
           <link rel="preload" href={fabricTexture.src} as="image" />
           <link rel="preload" href={gridTexture.src} as="image" />
           <meta name="theme-color" content="#fec301" />
+          <meta
+            name="facebook-domain-verification"
+            content="z6nna7jlyl6ehzowkxc3qp1oha3wb6"
+          />
+          <Script
+            id="facebook-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '764726835805460');
+            fbq('track', 'PageView');
+            `,
+            }}
+          />
+          <noscript>
+            <img
+              alt="facebook-pixel"
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=764726835805460&ev=PageView&noscript=1"
+            />
+          </noscript>
         </head>
         <body className={inter.className}>
           <div id="root">
@@ -91,8 +124,7 @@ export default async function LocaleLayout({
             </div>
           </div>
         </body>
-        <GoogleAnalytics gaId="GTM-T7H3RP3Q" />
-        {/* FIX: Make sure to test google Analistics */}
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ID!} />
       </NextIntlClientProvider>
     </html>
   );
